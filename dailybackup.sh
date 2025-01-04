fcb744b97a69e6a486bfa7820b75b0b5ba177671
Jens Stamenov <jensstam@yahoo.com>
2025-01-04 19:30:25 +0000

    changing daily backup script to run based off of save-off and save-on commands to prevent server from needing daily restart

--- dailybackup.sh
+++ dailybackup.sh
@@ -2,15 +2,16 @@
 
 # Define session and command to run in tmux
 SESSION_NAME="mcterm"
-STOP_COMMAND="stop"
+STOP_COMMAND="save-off"
 date
-echo "Sending stop command to tmux"
+echo "Turning off auto-saving to prevent corruption"
 tmux send-keys -t "$SESSION_NAME" "$STOP_COMMAND" C-m
+sleep 10
 
 #sleep for 120 seconds to allow server to shut down
-date
-echo "Sleep for 120 to allow server to shut down"
-sleep 120
+#date
+#echo "Sleep for 120 to allow server to shut down"
+#sleep 120
 
 date
 echo "Starting backup"
@@ -39,13 +40,14 @@ echo "Deleting backups older than 4 days"
 find "$BACKUP_DIR" -type f -name "*.tar.gz" -mtime +4 -delete
 
 
-#sleep 60 before starting server
+#sleep 10 before starting server
 date
-echo "Sleeping 60 before starting server"
-sleep 60
+echo "Sleep 10 before turning auto saving back on"
+sleep 10
 
 #new command to start server in tmux
-START_COMMAND="/home/opc/Spigot_Server/start.sh"
+#START_COMMAND="/home/opc/Spigot_Server/start.sh"
+START_COMMAND="save-on"
 tmux send-keys -t "$SESSION_NAME" "$START_COMMAND" C-m
 date
-echo "Server started"
+#echo "Server started"
