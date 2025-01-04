96fc869fb8edc285e35b8b67f1a92bb64fbd172a
Jens Stamenov <jensstam@yahoo.com>
2025-01-04 20:27:25 +0000

    creating restart script for shutting down server and checking if java process is still running after shutdown to determine whether to force restart or not

--- restart.sh
+++ restart.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
+
+#stop server
 SESSION_NAME="mcterm"
 COMMAND="stop"
-tmux send-keys -t "$SESSION_NAME" "$COMMAND" C-m
-sleep 30
+date
+echo "Stopping MC server"
+echo tmux send-keys -t "$SESSION_NAME" "$COMMAND" C-m
+sleep 5
 
+#kill tmux session
+date
+echo "Killing tmux session"
+echo tmux kill-session -t $SESSION_NAME
+sleep 5
+
+#check for java PID running and request to kill gracefully
+date
+echo "Checking for Java process running"
+if 
+    pgrep -x "java" > /dev/null 
+then 
+    echo "Requsting graceful kill for java PID"
+    echo "kill $(pgrep -x "java")"
+    sleep 60
+else 
+    echo "Java not running, no need to kill"
+fi
+
+
+# 
 START_COMMAND="/home/opc/Spigot_Server/start.sh"
-tmux send-keys -t "$SESSION_NAME" "$START_COMMAND" C-m
+echo tmux send-keys -t "$SESSION_NAME" "$START_COMMAND" C-m
 
 
